9c77773079b14adde20a0fc214cd8d24695532a7
ZenoMilk12 <xalexclashroyale@gmail.com>
2022-01-03 20:18:14 +0200

    am adaugat cursurile

--- cursuri.js
+++ cursuri.js
@@ -1,18 +1,42 @@
+const primul = document.getElementById('curs-primul')
+const experienta = document.getElementById('curs-experienta')
+
 function schimbareTitlu(id, titlu) {
+   console.log(`schimbare titlu se executa unde titlu '${titlu}'`);
     document.getElementById(id).innerText = titlu;
 }
 
-function schimbareSRCa(id, url) {
-    document.getElementById(id).innerHTML = "src=" + url;
+function schimba() {
+    aratat = !aratat
 }
 
-function cursContinua(numar) {
-    switch (numar) {
-        case 0:
-            schimbareSRC("curs", "https://streamable.com/amwvxm")
-        case 1:
-            schimbareTitlu("curs-info", "Cursul 2 : Experienta cu seful P2")
-            schimbareSRC("curs", "https://files.fm/u/ty9tpajpz")
+// function schimbareSRC(id, url) {
+//     document.getElementById(id).innerHTML = "src=" + url;
+// }
+
+// function cursContinua(text) {
+//     switch (text) {
+//         case 'primul':
+//             schimbareTitlu("curs-info", "Primul curs, baza :")
+//             schimbareSRC("curs", "https://streamable.com/amwvxm")
+//         case 'experienta':
+//             schimbareTitlu("curs-info", "Cursul 2 : Experienta cu seful P2")
+//             schimbareSRC("curs", "https://files.fm/u/ty9tpajpz")
+//     }
+// }
+
+show
+
+function cursContinua(text) {
+    if (text === "primul") {
+        schimbareTitlu("curs-info", "Primul curs, baza :")
+        experienta.style.display = "none"
+        primul.style.display = "block"
     }
+    else if (text === "experienta") {
+        schimbareTitlu("curs-info", "Cursul 2 : Experienta cu seful P2")
+        primul.style.display = "none"
+        experienta.style.display = "block"
+    }
+    else return
 }
-
