1618d87d97698668fb1c2daa6f68584688a73ace
ZenoMilk12 <xalexclashroyale@gmail.com>
2022-01-14 14:13:38 +0200

    adaugare presiunea si sacrificiile

--- cursuri.js
+++ cursuri.js
@@ -1,6 +1,7 @@
 const primul = document.getElementById('curs-primul')
 const experienta = document.getElementById('curs-experienta')
 const meniu = document.getElementById('curs-meniu')
+const doi = document.getElementById('curs-doi')
 
 function schimbareTitlu(id, titlu) {
     document.getElementById(id).innerText = titlu;
@@ -30,19 +31,29 @@ function cursContinua(text) {
         schimbareTitlu("curs-info", "Primul curs, baza :")
         meniu.style.display = "none"
         experienta.style.display = "none"
+        doi.style.display = "none"
         primul.style.display = "block"
     }
     else if (text === "experienta") {
         schimbareTitlu("curs-info", "Cursul 2 : Experienta cu seful P2")
         primul.style.display = "none"
         meniu.style.display = "none"
+        doi.style.display = "none"
         experienta.style.display = "block"
     }
     else if (text === "meniu") {
-        schimbareTitlu("curs-info", "Cursul 2 : Meniu cu seful")
+        schimbareTitlu("curs-info", "Cursul 3 : Meniu cu seful")
         experienta.style.display = "none"
         primul.style.display = "none"
+        doi.style.display = "none"
         meniu.style.display = "block"
+    }
+    else if (text === "doi") {
+        schimbareTitlu("curs-info", "Presiunea si sacrificiile :")
+        experienta.style.display = "none"
+        primul.style.display = "none"
+        meniu.style.display = "none"
+        doi.style.display = "block"
     }
     else return
 }
