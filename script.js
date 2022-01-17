5fccc99c7b725c60769ebaf626e449598a672341
ZenoMilk12 <xalexclashroyale@gmail.com>
2022-01-17 20:51:40 +0200

    cod buguit

--- script.js
+++ script.js
@@ -1,3 +1,27 @@
 function dute(url) {
     window.location.href = url
 }
+// Buggy code
+// function verificaPromo(promo) {
+//     var promocode = promo.value
+//     const promocodes = ["andrei"]
+//     if (promocodes.includes(promocode)) {
+//         // If it is, return true
+//         return true
+//     }  else {
+//         // If it is not, return false
+//         return false
+//     }
+// }
+
+// function promo(promocode) {
+//     var promocode = promo.value
+//     if (verificaPromo(promocode) === true) {
+//         console.log(`Promocode pt` + ` ${promocode}` + ` a fost aplicat!`)
+//         dute(`promocode/${promocode}.html`)
+//     } else {
+//         console.log("Codul introdus nu exista!")
+//         promocode = ""
+//         return
+//     }
+// }
